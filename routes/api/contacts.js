291cd4a437253f84e7358bf243f6ac1d3be3321d
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-11-26 16:31:58 +0200

    com 1

---
+++ routes/api/contacts.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const router = express.Router()
+
+const ctrl = require('../../controllers/contacts')
+
+const {validateBody} = require('../../middlewares')
+
+const schemas = require('../../schemas/contacts')
+
+router.get('/', ctrl.getAll )
+
+router.get('/:id', ctrl.getById)
+
+router.post('/', validateBody(schemas.addSchema), ctrl.add)
+
+router.delete('/:id', ctrl.delById)
+
+router.put('/:id', validateBody(schemas.addSchema), ctrl.updateById)
+
+module.exports = router
